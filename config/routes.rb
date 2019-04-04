d772e6b5cb534465063dffb606dd2e8d6dfebd3d
Damon Chivers <chiversdamon@gmail.com>
2019-04-03 18:15:32 -0600

    all routes added, a few notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # get 'departments/index'
+  # get 'departments/create'
+  # get 'departments/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # index
   # get '/employees', to: "employees#index"
-  resources :employees, only: [:index, :new, :create]
+  # resources :employees, only: [:index, :new, :create]
+  resources :departments
 end
---
+++ test/controllers/departments_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DepartmentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get departments_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get departments_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get departments_new_url
+    assert_response :success
+  end
+
+end
